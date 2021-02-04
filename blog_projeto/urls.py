80e897db210f725dec547d58f9b6789f238defb1
Jonathas Jivago <jonathas_jivago@hotmail.com>
2021-02-04 15:02:48 -0300

    Criação e configuração do app blog

--- blog_projeto/urls.py
+++ blog_projeto/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('paginas.urls'))
+    path('', include('paginas.urls')),
+    path('blog/', include('blog.urls'))
 ]
